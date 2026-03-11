27eef3d9eddd7bb3d7d0e2e0f4b77e6c990a54bd
snuny2 <82195651+snuny2@users.noreply.github.com>
2026-03-12 01:19:05 +0900

    두번째 수정

--- 00_web/backend/dist/me.controller.d.ts
+++
@@ -1,6 +0,0 @@
-import express from 'express';
-export declare class MeController {
-    me(req: express.Request): {
-        user: any;
-    };
-}
--- 00_web/backend/dist/users/user.entity.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class User {
-    id: number;
-    name: string;
-    userId: string;
-    passwordHash: string;
-    createAt: Date;
-    updateAt: Date;
-}
